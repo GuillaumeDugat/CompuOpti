53ebc00ce535a1ea493ec09651cfbbcb1153dac4
Sacha Muller <chloe.muller@student-cs.fr>
2023-01-29 20:04:30 +0100

    added computation of non dominated surface

---
+++ build_model.py
@@ -0,0 +1,292 @@
+import os
+import json
+
+import numpy as np
+import gurobipy as grb
+from gurobipy import GRB
+
+
+def build_model(data, with_epsilon_contraints=False,):
+    model = grb.Model()
+
+    worker_length = len(data["staff"])  # Number of workers
+    job_length = len(data["jobs"])  # Number of jobs
+    skill_length = len(data["qualifications"])  # Number of skills
+    day_length = data["horizon"]  # Number of days
+
+    # Define jobs parameters
+    gains_job = np.array([job["gain"] for job in data["jobs"]])
+    penalties_job = np.array([job["daily_penalty"] for job in data["jobs"]])
+    due_dates_job = np.array([job["due_date"] for job in data["jobs"]])
+    work_days_job_skill = np.array(
+        [
+            [
+                job["working_days_per_qualification"][skill]
+                if skill in job["working_days_per_qualification"]
+                else 0
+                for skill in data["qualifications"]
+            ]
+            for job in data["jobs"]
+        ]
+    )
+
+    # Define staff parameters
+    qualifications_worker_skill = np.array(
+        [
+            [
+                1 if skill in worker["qualifications"] else 0
+                for skill in data["qualifications"]
+            ]
+            for worker in data["staff"]
+        ]
+    )
+    vacations_worker_day = np.array(
+        [
+            [1 if 1 + day in worker["vacations"] else 0 for day in range(day_length)]
+            for worker in data["staff"]
+        ]
+    )
+
+    ## DECISION VARIABLES ##
+
+    # 4-D array of binary variables : 1 if a worker is assigned to a certain project for a certain skill on a certain day, else 0
+    works_worker_job_skill_day = model.addVars(
+        worker_length,
+        job_length,
+        skill_length,
+        day_length,
+        vtype=GRB.BINARY,
+        name="work",
+    )
+
+    is_realized_job = model.addVars(
+        job_length, vtype=GRB.BINARY, name="is_realized"
+    )  # 1 if a job is realized, else 0
+
+    is_worked_job_day = model.addVars(
+        job_length, day_length, vtype=GRB.BINARY, name="is_worked"
+    )  # 1 if a certain day is a work day for a certain job, else 0
+    max_duration = model.addVar(
+        vtype=GRB.INTEGER, name="max_duration"
+    )  # Integer that represents the maximum duration for any job
+
+    is_assigned_worker_job = model.addVars(
+        worker_length, job_length, vtype=GRB.BINARY, name="is_assigned"
+    )  # 1 if a certain worker is assigned on a certain job, else 0
+    max_assigned = model.addVar(
+        vtype=GRB.INTEGER, name="max_assigned"
+    )  # Integer that represents the maximum number of assigned jobs for any worker
+
+    model = add_constraints(
+        model,
+        worker_length,
+        job_length,
+        skill_length,
+        day_length,
+        work_days_job_skill,
+        qualifications_worker_skill,
+        vacations_worker_day,
+        works_worker_job_skill_day,
+        is_realized_job,
+        is_worked_job_day,
+        max_duration,
+        is_assigned_worker_job,
+        max_assigned,
+    )
+
+    model = add_objective(
+        model,
+        job_length,
+        day_length,
+        gains_job,
+        penalties_job,
+        due_dates_job,
+        is_realized_job,
+        is_worked_job_day,
+        max_duration,
+        max_assigned,
+        with_epsilon_contraints=with_epsilon_contraints,
+    )
+
+    return model
+
+
+def add_constraints(
+    model,
+    worker_length,
+    job_length,
+    skill_length,
+    day_length,
+    work_days_job_skill,
+    qualifications_worker_skill,
+    vacations_worker_day,
+    works_worker_job_skill_day,
+    is_realized_job,
+    is_worked_job_day,
+    max_duration,
+    is_assigned_worker_job,
+    max_assigned,
+):
+
+    model.addConstrs(
+        (
+            works_worker_job_skill_day[worker, job, skill, day]
+            <= qualifications_worker_skill[worker, skill]
+            for worker in range(worker_length)
+            for job in range(job_length)
+            for skill in range(skill_length)
+            for day in range(day_length)
+        ),
+        name="qualification",
+    )
+
+    model.addConstrs(
+        (
+            grb.quicksum(
+                works_worker_job_skill_day[worker, job, skill, day]
+                for job in range(job_length)
+                for skill in range(skill_length)
+            )
+            <= 1 - vacations_worker_day[worker, day]
+            for worker in range(worker_length)
+            for day in range(day_length)
+        ),
+        name="vacation",
+    )
+
+    model.addConstrs(
+        (
+            grb.quicksum(
+                works_worker_job_skill_day[worker, job, skill, day]
+                for worker in range(worker_length)
+                for day in range(day_length)
+            )
+            == is_realized_job[job] * work_days_job_skill[job, skill]
+            for job in range(job_length)
+            for skill in range(skill_length)
+        ),
+        name="job_coverage",
+    )
+
+    # exists_worker_skill works == 1 => is_worked == 1
+    model.addConstrs(
+        (
+            works_worker_job_skill_day[worker, job, skill, day]
+            <= is_worked_job_day[job, day]
+            for worker in range(worker_length)
+            for job in range(job_length)
+            for skill in range(skill_length)
+            for day in range(day_length)
+        ),
+        name="is_worked_job_day",
+    )
+    # forall_worker_skill works == 0 => is_worked == 0
+    model.addConstrs(
+        (
+            is_worked_job_day[job, day]
+            <= grb.quicksum(
+                works_worker_job_skill_day[worker, job, skill, day]
+                for worker in range(worker_length)
+                for skill in range(skill_length)
+            )
+            for job in range(job_length)
+            for day in range(day_length)
+        ),
+        name="is_worked_job_day_bis",
+    )
+
+    model.addConstrs(
+        (
+            grb.quicksum(is_worked_job_day[job, day] for day in range(day_length))
+            <= max_duration
+            for job in range(job_length)
+        ),
+        name="max_duration",
+    )
+
+    # exists_skill_day works == 1 => is_assigned == 1
+    model.addConstrs(
+        (
+            works_worker_job_skill_day[worker, job, skill, day]
+            <= is_assigned_worker_job[worker, job]
+            for worker in range(worker_length)
+            for job in range(job_length)
+            for skill in range(skill_length)
+            for day in range(day_length)
+        ),
+        name="is_assigned_worker_job",
+    )
+    # forall_skill_day works == 0 => is_assigned == 0
+    model.addConstrs(
+        (
+            is_assigned_worker_job[worker, job]
+            <= grb.quicksum(
+                works_worker_job_skill_day[worker, job, skill, day]
+                for skill in range(skill_length)
+                for day in range(day_length)
+            )
+            for worker in range(worker_length)
+            for job in range(job_length)
+        ),
+        name="is_assigned_worker_job_bis",
+    )
+
+    model.addConstrs(
+        (
+            grb.quicksum(
+                is_assigned_worker_job[worker, job] for job in range(job_length)
+            )
+            <= max_assigned
+            for worker in range(worker_length)
+        ),
+        name="max_assigned",
+    )
+
+    return model
+
+
+def add_objective(
+    model,
+    job_length,
+    day_length,
+    gains_job,
+    penalties_job,
+    due_dates_job,
+    is_realized_job,
+    is_worked_job_day,
+    max_duration,
+    max_assigned,
+    with_epsilon_contraints=False,
+):
+    # Add primary objective
+    model.setObjective(
+        grb.quicksum(
+            gains_job[job] * is_realized_job[job]
+            - penalties_job[job]
+            * grb.quicksum(
+                is_worked_job_day[job, day]
+                for day in range(due_dates_job[job], day_length)
+            )
+            for job in range(job_length)
+        )
+        + (
+            0.001 * max_duration + 0.001 * max_assigned
+            if with_epsilon_contraints
+            else 0
+        ),
+        sense=GRB.MAXIMIZE,
+    )
+
+    return model
+
+
+def get_instance(instance_filename):
+    file = os.path.join("instances", instance_filename)
+    data = json.load(open(file, "r"))
+    return data
+
+
+if __name__ == "__main__":
+    instance_filename = "toy_instance.json"
+    data = get_instance(instance_filename)
+    build_model(data)
---
+++ non_dominated_surface.py
@@ -0,0 +1,84 @@
+from gurobipy import GRB
+from build_model import build_model, get_instance
+
+
+def compute_non_dominated_surface(
+    model,
+    data,
+    max_assigned_name: str = "max_assigned",
+    max_duration_name: str = "max_duration",
+):
+    model.Params.LogToConsole = 0  # muting the output of model.optimize()
+
+    model.optimize()  # required to optimize to retrieve the variables in getVars
+    max_duration = model.getVarByName(max_duration_name)
+    max_assigned = model.getVarByName(max_assigned_name)
+
+    non_dominated_solutions = []
+
+    horizon = data["horizon"]  # max value max duration can take
+    total_nb_projects = len(data["jobs"])  # max value max assigned can take
+
+    epsilon_c_max_duration = horizon
+
+    while epsilon_c_max_duration >= 0:
+        print(epsilon_c_max_duration)
+
+        epsilon_c_max_assigned = total_nb_projects
+        next_epsilon_c_max_duration = 0
+
+        model.addConstr(
+            (max_duration <= epsilon_c_max_duration),
+            name=f"{max_duration_name}_epsilon",
+        )
+
+        while epsilon_c_max_assigned >= 0:
+
+            model.addConstr(
+                (max_assigned <= epsilon_c_max_assigned),
+                name=f"{max_assigned_name}_epsilon",
+            )
+
+            model.optimize()
+
+            model.remove(model.getConstrByName(f"{max_assigned_name}_epsilon"))
+
+            if model.Status == GRB.OPTIMAL:
+                solutions_variable = build_variables_dictionnary(model)
+                non_dominated_solutions.append(solutions_variable)
+
+                next_epsilon_c_max_duration = max(
+                    solutions_variable[max_duration_name], next_epsilon_c_max_duration
+                )
+                epsilon_c_max_assigned = solutions_variable[max_assigned_name] - 1
+
+            elif model.Status == GRB.INFEASIBLE:
+                break
+            elif model.Status == GRB.TIME_LIMIT:
+                raise ValueError(
+                    "Epsilon constraint method failed because of timeout. We recommend increasing the time limit."
+                )
+
+        model.remove(model.getConstrByName(f"{max_duration_name}_epsilon"))
+
+        epsilon_c_max_duration = next_epsilon_c_max_duration - 1
+
+    model.Params.LogToConsole = 1
+
+    return non_dominated_solutions
+
+
+def build_variables_dictionnary(model):
+    variables = {}
+    for v in model.getVars():
+        variables[v.VarName] = v.X
+    variables["objVal"] = model.objVal
+    return variables
+
+
+if __name__ == "__main__":
+    instance_filename = "toy_instance.json"
+    data = get_instance(instance_filename)
+    model = build_model(data, with_epsilon_contraints=True)
+    non_dominated_models = compute_non_dominated_surface(model, data)
+    print(non_dominated_models)
